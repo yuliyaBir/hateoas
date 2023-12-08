dbb906fceeec898a3d587ca5bbacdf34835c8958
yuliyaBir <zhivo4ka@gmail.com>
2023-12-08 14:41:34 +0100

    Initial commit

---
+++ src/main/java/be/vdab/hateoas/ConsoleRepository.java
@@ -0,0 +1,2 @@
+package be.vdab.hateoas;public class ConsoleRepository {
+}
---
+++ src/test/java/be/vdab/hateoas/HateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
